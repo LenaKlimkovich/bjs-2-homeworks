b697c9ad51695549c32b6cb2ee9dd1b978165224
LKl <plat.elena2010@ya.ru>
2025-11-11 14:36:13 +0300

    Task2 Final

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,15 +2,11 @@
 function solveEquation(a, b, c) {
 	let arr = [];
 	let d = b ** 2 - 4 * a * c;
-	let root1;
-	let root2;
-	if (d < 0) {
-		return arr;
-	} else if (d === 0) {
+	if (d === 0) {
 		arr.push(-b / (2 * a))
 	} else if (d > 0) {
-		root1 = (-b + Math.sqrt(d)) / (2 * a);
-		root2 = (-b - Math.sqrt(d)) / (2 * a);
+		let root1 = (-b + Math.sqrt(d)) / (2 * a);
+	    let root2 = (-b - Math.sqrt(d)) / (2 * a);
 		arr.push(root1, root2);
 	}
 	return arr;
