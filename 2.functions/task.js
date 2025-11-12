ca482c61019b014f95cc776de02ae42e0035bae2
LKl <plat.elena2010@ya.ru>
2025-11-12 10:11:57 +0300

    Ininitial commit Task1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,14 @@
 function getArrayParams(...arr) {
-
+let min = arr[0];
+let max = arr[0];
+let sum = 0;
+Math.max(arr);
+Math.min(arr);
+sum = arr.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  sum,
+);
+let avg = Number((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
