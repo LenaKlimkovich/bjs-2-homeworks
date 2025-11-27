53e0642f5e2f8e319cd8af5c0e52ad5acb5a4458
LKl <plat.elena2010@ya.ru>
2025-11-27 10:49:36 +0300

    Initial

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,48 @@
-﻿
+﻿function parseCount(value) {
+let result = Number.parseFloat(value);
+  if (Number.isNaN(result)) {
+    throw new Error("Невалидное значение");
+  }
+  return result;
+}
+
+function validateCount(value) {
+  try {
+    return parseCount(value);
+  } catch (error) {
+    return error;
+  }
+}
+
+
+    * Аргументами функции являются три значения длин сторон.
+    * Попытайтесь вернуть новый объект треугольника.
+    * В случае перехвата исключения возвращайте объект с двумя геттерами `area` и `perimeter`, которые возвращают строку: *«Ошибка! Треугольник не существует»*.
+class Triangle {
+    constructor(side1, side2, side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+        if (this.side1 + this.side2 < this.side3 || this.side2 + this.side3 < this.side1 || this.side1 + this.side3 < this.side2) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+    }
+    get perimeter() {
+        return this.side1 + this.side2 + this.side3;
+    }
+    get area() {
+        let halfPer = this.perimeter / 2
+        return Number(Math.sqrt(halfPer * (halfPer - this.side1) * 
+        (halfPer - this.side2) * (halfPer - this.side3)).toFixed(3))
+    }
+}
+   function getTriangle(side1, side2, side3) {
+     try {
+         return new Triangle(side1, side2, side3)
+     } catch (error) {
+         return {
+             perimeter: "Ошибка! Треугольник не существует",
+             area: "Ошибка! Треугольник не существует"
+         };
+     }
+ }
